8d049abb8d6f1edd2b21e7be14fba6fc8bb05aa8
Chinmay Soni <61123785+heyamsoni@users.noreply.github.com>
2025-05-27 23:42:55 +0300

    Updated texts on gui-window.py

--- gui-window.py
+++ gui-window.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
 def say_hello():
-    print("Hello from Anatolia GUI!")
+    print("Hello this is simple GUI!")
 
 root = tk.Tk()
-root.title("Anatolia Sample GUI")
+root.title("Tkinter GUI Python")
 
 button = tk.Button(root, text="Click Me", command=say_hello)
 button.pack()
